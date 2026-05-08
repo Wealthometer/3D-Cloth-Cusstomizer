26ff9b8055b636f2fa981cf7528d156e35de7abc
Wealthometer <wealthhajoh87@gmail.com>
2026-05-08 19:59:03 +0100

    Update Customizer.jsx

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -2,3 +2,4 @@ import React, { useState, useEffect } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
+import config from '../config/config';
