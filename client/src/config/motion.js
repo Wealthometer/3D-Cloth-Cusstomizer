1736955160a5db4ff7e363f392e0a203bf14d841
Wealthometer <wealthhajoh87@gmail.com>
2026-05-08 18:35:07 +0100

    Update motion.js

--- client/src/config/motion.js
+++ client/src/config/motion.js
@@ -2,3 +2,4 @@ export const transition = { type: "spring", duration: 0.8 };
 
 export const slideAnimation = (direction) => {
   return {
+      x: direction === "left" ? -100 : direction === "right" ? 100 : 0,
