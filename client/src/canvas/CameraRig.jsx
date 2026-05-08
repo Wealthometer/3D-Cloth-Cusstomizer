09ed29f78997f5ab5b0ada6cf72d51c2288eed72
Wealthometer <wealthhajoh87@gmail.com>
2026-05-08 17:48:50 +0100

    Update CameraRig.jsx

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -16,3 +16,4 @@ const CameraRig = ({ children }) => {
     // set the initial position of the model
     let targetPosition = [-0.4, 0, 2];
     if(snap.intro) {
+      if(isBreakpoint) targetPosition = [0, 0, 2];
