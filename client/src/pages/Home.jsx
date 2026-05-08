2fa4da61bc6af1e267cf8d5920ec1065d1f929c9
Wealthometer <wealthhajoh87@gmail.com>
2026-05-08 18:47:04 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -44,3 +44,4 @@ const Home = () => {
                 title="Customize It"
                 handleClick={() => state.intro = false}
                 customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
