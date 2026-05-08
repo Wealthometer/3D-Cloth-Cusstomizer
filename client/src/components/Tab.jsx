9218dcde9f24f5a0b68cfb4798985b26bac9f076
Wealthometer <wealthhajoh87@gmail.com>
2026-05-08 18:21:50 +0100

    Update Tab.jsx

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -21,3 +21,4 @@ const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
         src={tab.icon}
         alt={tab.name}
         className={`${isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12 object-contain'}`}
+      />
