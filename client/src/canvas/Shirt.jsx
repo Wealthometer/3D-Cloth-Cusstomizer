22de4d0a32a61f2219f548886abde0fbc504b35b
Wealthometer <wealthhajoh87@gmail.com>
2026-05-08 18:08:55 +0100

    Update Shirt.jsx

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -4,4 +4,5 @@ import { useSnapshot } from 'valtio';
 import { useFrame } from '@react-three/fiber';
 import { Decal, useGLTF, useTexture } from '@react-three/drei';
 
+import state from '../store';
 const Shirt = () => {
