4dc9204095bbf40720375313c167a6d46217436e
Wealthometer <wealthhajoh87@gmail.com>
2026-05-08 17:50:11 +0100

    Update index.jsx

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -14,3 +14,4 @@ const CanvasModel = () => {
       className="w-full max-w-full h-full transition-all ease-in"
     >
       <ambientLight intensity={0.5} />
+      {/* <Environment preset="studio" /> */}
