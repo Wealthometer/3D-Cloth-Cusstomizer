043d39a571748ba05b6db5daf72f7d7441c9cb77
Wealthometer <wealthhajoh87@gmail.com>
2026-05-08 18:25:45 +0100

    Update helpers.js

--- client/src/config/helpers.js
+++ client/src/config/helpers.js
@@ -14,3 +14,4 @@ export const reader = (file) =>
   new Promise((resolve, reject) => {
     const fileReader = new FileReader();
     fileReader.onload = () => resolve(fileReader.result);
+    fileReader.readAsDataURL(file);
