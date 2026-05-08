c41021ee3373319e07a959a2be17ae62b9fa67bf
Wealthometer <wealthhajoh87@gmail.com>
2026-05-08 18:34:13 +0100

    Update helpers.js

--- client/src/config/helpers.js
+++ client/src/config/helpers.js
@@ -27,3 +27,4 @@ export const getContrastingColor = (color) => {
   const b = parseInt(hex.substring(4, 6), 16);
 
   // Calculate the brightness of the color
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
