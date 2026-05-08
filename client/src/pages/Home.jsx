781427eb512dbf495e40c06da199bba7adfb1a8f
Wealthometer <wealthhajoh87@gmail.com>
2026-05-08 18:46:48 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -39,3 +39,4 @@ const Home = () => {
               Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
               </p>
 
+              <CustomButton 
