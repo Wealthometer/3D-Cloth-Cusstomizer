ac10c39a1ceace06e6db56a8774a56744adfa368
Wealthometer <wealthhajoh87@gmail.com>
2026-05-08 18:47:09 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -45,3 +45,4 @@ const Home = () => {
                 handleClick={() => state.intro = false}
                 customStyles="w-fit px-4 py-2.5 font-bold text-sm"
               />
+            </motion.div>
