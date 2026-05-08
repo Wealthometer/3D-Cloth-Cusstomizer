823f32beedebe436b79761a31aed34937809f85a
Wealthometer <wealthhajoh87@gmail.com>
2026-05-08 17:43:13 +0100

    Update index.js

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -6,3 +6,4 @@ import download from "./download.png";
 import logoShirt from "./logo-tshirt.png";
 import stylishShirt from "./stylish-tshirt.png";
 
+export { ai, fileIcon, swatch, download, logoShirt, stylishShirt };
