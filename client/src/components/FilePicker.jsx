dd91051d96d0151d4a3472ece88e96cb337bebb9
Wealthometer <wealthhajoh87@gmail.com>
2026-05-08 18:18:26 +0100

    Update FilePicker.jsx

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -5,3 +5,4 @@ import CustomButton from './CustomButton'
 const FilePicker = ({ file, setFile, readFile }) => {
   return (
     <div className="filepicker-container">
+      <div className="flex-1 flex flex-col">
