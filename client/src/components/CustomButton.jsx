6a43096308a289c0caf71d15495f62683536f7a2
Wealthometer <wealthhajoh87@gmail.com>
2026-05-08 18:17:40 +0100

    Update CustomButton.jsx

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -25,3 +25,4 @@ const CustomButton = ({ type, title, customStyles, handleClick }) => {
   return (
     <button
       className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
