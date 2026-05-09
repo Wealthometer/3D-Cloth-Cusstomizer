12a9549a7766de355b6a7ea68313a465e59797df
Wealthometer <wealthhajoh87@gmail.com>
2026-05-09 13:50:31 +0100

    Update Customizer.jsx

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -149,3 +149,4 @@ const Customizer = () => {
               title="Go Back"
               handleClick={() => state.intro = true}
               customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
